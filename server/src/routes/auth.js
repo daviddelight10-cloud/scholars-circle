fbaf399dde1729763e5278f689d7386a20c43f2c
delight zibiri-david <daviddelight10@gmail.com>
2026-05-08 12:43:57 -0700

    Fix missing requireAuth import in auth routes

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -10,6 +10,8 @@ import { prisma } from "../db.js";
 
 import { generateActivationKey } from "./keys.js";
 
+import { requireAuth } from "../middleware/auth.js";
+
 
 
 const router = express.Router();
